f45948353a10e6bc9080209dcfdb061176e45732
Gabe Witcher <gabewitcher@gmail.com>
2025-03-21 17:46:11 -1000

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by Gabe Witcher on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
